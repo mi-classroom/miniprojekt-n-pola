f9d2a5943c90376bfa973d9dfb214685dbb934f8
n-pola <n.polarek@live.de>
2020-07-24 17:04:16 +0200

    add function to control Modal w/ keyboard

--- cranach-miniprojekt/src/App.vue
+++ cranach-miniprojekt/src/App.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
 import Header from '@/components/Header.vue';
 
 export default {
@@ -13,13 +14,33 @@ export default {
   components: {
     Header,
   },
+  computed: {
+    ...mapState({
+      open: (state) => state.modalOpen,
+    }),
+  },
   data() {
     return {
       lang: 'de',
     };
   },
+  methods: {
+    ...mapActions(['closeModal', 'modalPrevImage', 'modalNextImage'])
+  },
   beforeMount() {
     this.$store.dispatch('loadPaintings', `/data/json/cda-paintings-v2.${this.lang}.json`);
+  },
+  created() {
+    window.addEventListener('keyup', (e) => {
+      console.log(e.key);
+      if (e.key === 'Escape') {
+        this.closeModal();
+      } else if (e.key === 'ArrowRight' && this.open) {
+        this.modalNextImage();
+      } else if (e.key === 'ArrowLeft' && this.open) {
+        this.modalPrevImage();
+      }
+    });
   },
 };
 </script>
