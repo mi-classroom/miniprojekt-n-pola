7488b8fd473e4f45cd4809cbd1dfea350c27b9be
n-pola <n.polarek@live.de>
2020-07-24 16:55:30 +0200

    add hover to like button

--- cranach-miniprojekt/src/components/ModalNavigation.vue
+++ cranach-miniprojekt/src/components/ModalNavigation.vue
@@ -1,7 +1,11 @@
 <template>
   <nav class="modal-nav">
     <i class="icon icon--m modal-nav__icon" @click="modalPrevImage">arrow_back</i>
-    <i class="icon icon--m modal-nav__icon">favorite_border</i>
+    <span class="modal-nav__like modal-nav__icon" @mouseover="hoverLike = true"
+    @mouseleave="hoverLike = false">
+      <i v-if="!hoverLike" class="icon icon--m modal-nav__icon">favorite_border</i>
+      <i v-else class="icon icon--m modal-nav__icon modal-nav__icon--like">favorite</i>
+    </span>
     <i class="icon icon--m modal-nav__icon" @click="modalNextImage">arrow_forward</i>
   </nav>
 </template>
@@ -11,6 +15,11 @@ import { mapActions } from 'vuex';
 
 export default {
   name: 'modalNav',
+  data() {
+    return {
+      hoverLike: false,
+    };
+  },
   methods: {
     ...mapActions(['modalNextImage', 'modalPrevImage']),
   },
@@ -29,6 +38,9 @@ export default {
     cursor: pointer;
     &:not(:last-child) {
       margin-right: $xs;
+    }
+    &--like {
+      color: $accent-bright;
     }
   }
 }
